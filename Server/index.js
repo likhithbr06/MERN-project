d45d7d842f8c197e53b2a86191904557481aa7c7
Likhith <likhithbr06@gmail.com>
2022-11-03 11:09:03 +0530

    controllers,models  created

--- Server/index.js
+++ Server/index.js
@@ -3,16 +3,19 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import bodyparser from 'body-parser'
 import mongodb from 'mongodb'
+import postRoutes from './routes/post.js'
 
 const app = express();
+
+app.use('/posts',postRoutes)
 
 app.use(bodyparser.json({limit: "30mb",extended :"true"}))
 app.use(bodyparser.urlencoded({limit: "30mb",extended :"true"}))
 app.use(cors());
 
 const MongoClient = mongodb.MongoClient
-
-const url = 'mongodb://127.0.0.1:27017'
+//const MongoClient = mongoose.MongoClient
+const url = 'mongodb://127.0.0.1:27017/MERN'
 const PORT = process.env.PORT || 5000
 
 MongoClient.connect(url,{useNewUrlParser : true, useUnifiedTopology : true})
